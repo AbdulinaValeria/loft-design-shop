5acaa20ecf1c508e9731e0c6b4883493b5efa546
AbdulinaValeria <Valeriia.Abdulina.knm.2020@edu.lpnu.ua>
2021-12-11 17:57:03 +0200

    Initial commit

--- js/burger.js
+++ js/burger.js
@@ -1,33 +1,34 @@
-(function() {
+(function () {
+  const openMenuButton = document.querySelector(".openMenu");
+  const closeMenuButton = document.querySelector(".closeMenu");
+  const mainMenu = document.querySelector(".header-nav");
 
-    const openMenuButton = document.querySelector('.openMenu');
-    const closeMenuButton = document.querySelector('.closeMenu');
-    const mainMenu = document.querySelector('.header-nav');
-    
-    openMenuButton.addEventListener('click', showMenu);
-    closeMenuButton.addEventListener('click', closeMenu);
-    
-    function showMenu() {
-        openMenuButton.style.display = 'none';
-        closeMenuButton.style.display = 'block';
-        mainMenu.style.display = 'block';
+  openMenuButton.addEventListener("click", showMenu);
+  closeMenuButton.addEventListener("click", closeMenu);
+
+  function showMenu() {
+    openMenuButton.style.display = "none";
+    closeMenuButton.style.display = "block";
+    mainMenu.style.display = "block";
+    document.body.style.overflow = "hidden";
+  }
+
+  function closeMenu() {
+    openMenuButton.style.display = "block";
+    closeMenuButton.style.display = "none";
+    mainMenu.style.display = "none";
+    document.body.style.overflow = "scroll";
+  }
+
+  function showBurgerControls() {
+    if (window.innerWidth < 770) {
+      openMenuButton.style.display = "block";
+      mainMenu.style.display = "none";
+    } else {
+      openMenuButton.style.display = "none";
+      mainMenu.style.display = "block";
     }
-    
-    function closeMenu() {
-        openMenuButton.style.display = 'block';
-        closeMenuButton.style.display = 'none';
-        mainMenu.style.display = 'none';
-    }
-    
-    function showBurgerControls() {
-        if (window.innerWidth < 700) {
-            openMenuButton.style.display = 'block';
-            mainMenu.style.display = 'none';
-        } else {
-            openMenuButton.style.display = 'none';
-            mainMenu.style.display = 'block';
-        }
-    }
-    showBurgerControls();
-    window.addEventListener('resize', showBurgerControls);
+  }
+  showBurgerControls();
+  window.addEventListener("resize", showBurgerControls);
 })();
